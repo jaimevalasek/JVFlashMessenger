6d42b7036badaa7a144edb23eff0cac122c059b0
jaimevalasek <jaime.valasek@gmail.com>
2013-04-15 12:30:42 -0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -6,23 +6,23 @@ class Module
 {
     public function getServiceConfig() {
         return array(
-	        'factories' => array(
-				'jv_flashmessenger' => function ($sm) {
-	    			$viewModel = $sm->get('view_manager');
-	    			$controller = $sm->get('ControllerPluginManager');
-	    		
-	    			// Configurando variáveis
-	    			$flashMessenger = $controller->get('FlashMessenger');
-	    		
-	    			$viewModel = $viewModel->getViewModel();
-	    			$messages = $flashMessenger->getMessages();
-	    		
-	    			// Setando variáveis
-	    			$viewModel->setVariable('messages', $messages);
-	    		
-	    			return $viewModel;
-	    		},
-			)
-		);
-	}
+	    'factories' => array(
+	        'jv_flashmessenger' => function ($sm) {
+		    $viewModel = $sm->get('view_manager');
+		    $controller = $sm->get('ControllerPluginManager');
+		    	
+		    // Configurando variáveis
+		    $flashMessenger = $controller->get('FlashMessenger');
+		    
+		    $viewModel = $viewModel->getViewModel();
+		    $messages = $flashMessenger->getMessages();
+		    
+		    // Setando variáveis
+		    $viewModel->setVariable('messages', $messages);
+		    
+		    return $viewModel;
+		},
+            )
+        );
+    }
 }
