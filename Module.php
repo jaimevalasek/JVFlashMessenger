53123ac7dc07ca315ce76038f6dd50f48c91a0e6
jaimevalasek <jaime.valasek@gmail.com>
2013-04-15 10:27:31 -0300

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,26 @@
+namespace JVFlashMessenger;
+
+class Module
+{
+    public function getServiceConfig() {
+		return array(
+			'factories' => array(
+				'jv_flashmessenger' => function ($sm) {
+	    			$viewModel = $sm->get('view_manager');
+	    			$controller = $sm->get('ControllerPluginManager');
+	    		
+	    			// Configurando variáveis
+	    			$flashMessenger = $controller->get('FlashMessenger');
+	    		
+	    			$viewModel = $viewModel->getViewModel();
+	    			$messages = $flashMessenger->getMessages();
+	    		
+	    			// Setando variáveis
+	    			$viewModel->setVariable('messages', $messages);
+	    		
+	    			return $viewModel;
+	    		},
+			)
+		);
+	}
+}
