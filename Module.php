c2f85cfcc8a20c9cbdb06dc2c169589b73fa57cf
Jaime Marcelo Valasek <jaime.valasek@gmail.com>
2013-05-09 14:40:25 -0300

    Otimizando o código

--- Module.php
+++ Module.php
@@ -1,28 +1,21 @@
-<?php 
+<?php
 
 namespace JVFlashMessenger;
 
 class Module
 {
-    public function getServiceConfig() {
-        return array(
-	    'factories' => array(
-	        'jv_flashmessenger' => function ($sm) {
-		    $viewModel = $sm->get('view_manager');
-		    $controller = $sm->get('ControllerPluginManager');
-		    	
-		    // Configurando variáveis
-		    $flashMessenger = $controller->get('FlashMessenger');
-		    
-		    $viewModel = $viewModel->getViewModel();
-		    $messages = $flashMessenger->getMessages();
-		    
-		    // Setando variáveis
-		    $viewModel->setVariable('messages', $messages);
-		    
-		    return $viewModel;
-		},
-            )
-        );
-    }
+	public function getServiceConfig() {
+		return array(
+			'factories' => array(
+				'jv_flashmessenger' => function ($sm) {
+					// Setando variáveis
+					$viewModel = $sm->get('view_manager')->getViewModel();
+					$controller = $sm->get('ControllerPluginManager');
+					$flashMessenger = $controller->get('FlashMessenger');
+					
+					return $viewModel->setVariable('messages', $flashMessenger->getMessages());
+	    		},
+			)
+		);
+	}
 }
